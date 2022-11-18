8a44b928ae8a92e04d40c7e06bdd98b0933749d6
shaonianzhetnan <5294022+shaonianzhentan@users.noreply.github.com>
2022-11-18 23:29:46 +0800

    update

--- custom_components/ha_app/__init__.py
+++ custom_components/ha_app/__init__.py
@@ -8,13 +8,14 @@ import urllib.parse
 import logging, json, time, uuid
 
 from .manifest import manifest
-from .const import DOMAIN
+from .http import HttpView
 
 _LOGGER = logging.getLogger(__name__)
 CONFIG_SCHEMA = cv.deprecated(DOMAIN)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     config = entry.data
+    hass.http.register_view(HttpView)
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:

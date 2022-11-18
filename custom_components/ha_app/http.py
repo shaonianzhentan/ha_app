714a7535039bd75fb14fa6bda8f719168823a59b
shaonianzhentan <ilovevar@gmail.com>
2022-11-18 18:17:23 +0800

    支持推送

--- custom_components/ha_app/http.py
+++ custom_components/ha_app/http.py
@@ -7,6 +7,8 @@ class HttpView(HomeAssistantView):
     url = "/api/haapp"
     name = "api:haapp"
     requires_auth = False
+    # 计数器
+    count = 0
 
     async def post(self, request):
         hass = request.app["hass"]
@@ -21,10 +23,19 @@ class HttpView(HomeAssistantView):
             'title': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) if title is None else title,
             'message': message,
         }
+        dev_id = push_token
+        notification_id = f'{dev_id}{self.count}'
+        self.count = self.count + 1
+        if self.count > 50:
+            self.count = 0
 
         hass.loop.create_task(hass.services.async_call('persistent_notification', 'create', {
                     'title': message,
                     'message': json.dumps(result),
-                    'notification_id': f'ha_app{result["id"]}'
+                    'notification_id': notification_id
                 }))
+
+        result['id'] = notification_id
+        hass.bus.fire(dev_id, result)
+
         return self.json_message("推送成功", status_code=201)
